6aeda39b79e754bbc0a4228f47e3e3ccc842cffb
Sudhanshu Singh <sudhanshus026@gmail.com>
2026-03-16 23:46:28 +0530

    initialize activity service base structure

---
+++ activityservice/src/main/java/com/trainai/activityservice/service/ActivityService.java
@@ -0,0 +1,48 @@
+package com.trainai.activityservice.service;
+
+import com.trainai.activityservice.repository.ActivityRepository;
+import com.trainai.activityservice.dto.ActivityRequest;
+import com.trainai.activityservice.dto.ActivityResponse;
+import com.trainai.activityservice.model.Activity;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ActivityService {
+
+    private final ActivityRepository activityRepository;
+
+    private ActivityResponse mapToResponse(Activity activity){
+        ActivityResponse response = new ActivityResponse();
+        response.setId(activity.getId());
+        response.setUserId(activity.getUserId());
+        response.setType(activity.getType());
+        response.setDuration(activity.getDuration());
+        response.setCaloriesBurned(activity.getCaloriesBurned());
+        response.setStartTime(activity.getStartTime());
+        response.setAdditionalMetrics(activity.getAdditionalMetrics());
+        response.setCreatedAt(activity.getCreatedAt());
+        response.setUpdatedAt(activity.getUpdatedAt());
+        return response;
+    }
+
+    public List<ActivityResponse> getUserActivities(String userId) {
+        List<Activity> activities = activityRepository.findByUserId(userId);
+        return activities.stream()
+                .map(this::mapToResponse)
+                .collect(Collectors.toList());
+    }
+
+    public ActivityResponse getActivityById(String activityId) {
+        return activityRepository.findById(activityId)
+                .map(this::mapToResponse)
+                .orElseThrow(() -> new RuntimeException("Activity not found with id: " + activityId));
+    }
+}
